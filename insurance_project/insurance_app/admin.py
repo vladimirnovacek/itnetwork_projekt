f177e8cc26fa092eee5a51bcf2c6ed2f69854091
vladimirnovacek <novacek.vladimir@gmail.com>
2023-03-04 20:43:31 +0100

    PersonAdmin added. Adding new person in admin stil not working.

--- insurance_project/insurance_app/admin.py
+++ insurance_project/insurance_app/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from . import models
 
-admin.register(models.Person)
-admin.site.register(models.Person)
+
+class PersonAdmin(UserAdmin):
+    list_display = ('email', 'date_of_birth', 'is_superuser')
+    list_filter = ('is_superuser',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Permissions', {'fields': ('is_superuser',)}),
+    )
+    ordering = ('email',)
+
+
+admin.site.register(models.Person, PersonAdmin)
 admin.site.register(models.Contract)
 admin.site.register(models.Product)
+admin.site.unregister(Group)
