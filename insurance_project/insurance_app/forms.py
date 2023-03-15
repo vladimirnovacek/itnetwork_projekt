36dbb8d9024dac73d73ebd16fef9236adaa05e11
vladimirnovacek <novacek.vladimir@gmail.com>
2023-03-15 20:35:38 +0100

    Appearance changes

--- insurance_project/insurance_app/forms.py
+++ insurance_project/insurance_app/forms.py
@@ -2,6 +2,7 @@
 Module containing form classes of the insurance_app
 """
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import DateField
 
 from . import models
 
@@ -10,6 +11,8 @@ class RegisterPersonForm(UserCreationForm):
     """
     Form for creating a new person object
     """
+    date_of_birth = DateField(input_formats=['%d.%m.%Y'])
+
     class Meta:
         model = models.Person
         fields = ['email', 'password1', 'password2', 'first_name', 'last_name', 'date_of_birth', 'phone', 'address1',
