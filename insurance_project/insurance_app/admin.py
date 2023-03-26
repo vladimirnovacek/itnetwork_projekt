568a39aead13facdb163dff36d632fc3dc8a0c9f
vladimirnovacek <novacek.vladimir@gmail.com>
2023-03-26 11:30:11 +0200

    Changed some colors

--- insurance_project/insurance_app/admin.py
+++ insurance_project/insurance_app/admin.py
@@ -14,15 +14,15 @@ class PersonAdmin(UserAdmin):
     Encapsulate all admin options and functionality for a given model.
     """
     list_display: tuple = ('email', 'date_of_birth', 'is_superuser')
-    list_filter: tuple = ('is_superuser',)
+    list_filter: tuple = ('is_superuser', 'is_staff')
     fieldsets: tuple = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Personal info', {'fields': ('date_of_birth', 'first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_superuser', 'is_staff')}),
     )
     add_fieldsets = (
         (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
-        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Personal info', {'fields': ('date_of_birth', 'first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_superuser', 'is_staff')}),
     )
     ordering = ('email',)
