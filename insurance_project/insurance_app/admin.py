ceddee81e59b77099b9fada96dee062682cbfd44
vladimirnovacek <novacek.vladimir@gmail.com>
2023-03-26 17:28:51 +0200

    Altered a form so the last_name field is optional

--- insurance_project/insurance_app/admin.py
+++ insurance_project/insurance_app/admin.py
@@ -1,18 +1,29 @@
 """
 Settings of the admin page
 """
-
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import Group
 
 from . import models
+
+
+class OptionalLastNameUserCreationForm(UserCreationForm):
+    last_name = forms.CharField(required=False, label='Příjmení')
+
+
+class OptionalLastNameUserChangeForm(UserChangeForm):
+    last_name = forms.CharField(required=False, label='Příjmení')
 
 
 class PersonAdmin(UserAdmin):
     """
     Encapsulate all admin options and functionality for a given model.
     """
+    form = OptionalLastNameUserChangeForm
+    add_form = OptionalLastNameUserCreationForm
     list_display: tuple = ('email', 'date_of_birth', 'is_superuser')
     list_filter: tuple = ('is_superuser', 'is_staff')
     fieldsets: tuple = (
